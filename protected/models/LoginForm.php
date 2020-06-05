bbc0eadf711e3247bb90e02393044ae5b9de401f
ABHISHEK SHARMA <50879079+abhisheksharma28@users.noreply.github.com>
2020-06-05 08:16:59 +0530

    Final Commit

---
+++ protected/models/LoginForm.php
@@ -0,0 +1,100 @@
+<?php
+
+/**
+ * LoginForm class.
+ * LoginForm is the data structure for keeping
+ * user login form data. It is used by the 'login' action of 'SiteController'.
+ */
+class LoginForm extends CFormModel
+{
+	public $user_name;
+	public $user_password;
+	public $rememberMe;
+
+	private $_identity;
+
+	/**
+	 * Declares the validation rules.
+	 * The rules state that username and password are required,
+	 * and password needs to be authenticated.
+	 */
+	public function rules()
+	{
+		return array(
+			// username and password are required
+			array('user_name, user_password', 'required'),
+			// rememberMe needs to be a boolean
+			array('rememberMe', 'boolean'),
+			// password needs to be authenticated
+			array('user_password', 'authenticate'),
+		);
+	}
+
+	/**
+	 * Declares attribute labels.
+	 */
+	public function attributeLabels()
+	{
+		return array(
+			'rememberMe'=>'Remember me next time',
+		);
+	}
+
+	/**
+	 * Authenticates the password.
+	 * This is the 'authenticate' validator as declared in rules().
+	 * @param string $attribute the name of the attribute to be validated.
+	 * @param array $params additional parameters passed with rule when being executed.
+	 */
+	public function authenticate($attribute,$params)
+	{
+		$users_name = $this->user_name;
+		// print_r($users_name);
+		// echo $users_name;
+		// // $cond = "users_name = ".$users_name"";
+		$model=new Users;
+		 $user = Users::model()->find(array(
+			 'select'=>['users_id','users_name','users_email','users_password'],
+			 'condition'=>'users_name="'.$users_name.'"',
+
+		 ));
+		//print_r($user);
+
+
+		// die();
+	if($user === NULL)
+	 {
+		echo '<script>alert("Login FailedInvalid Username")</script>';
+	 } //
+	 else if($user->users_password===$this->user_password)
+	 {
+		 $url="http://localhost/Assignment_2-master/index.php?r=users/view&id=".($user->users_id);
+		 header("Location:$url");
+
+	 }
+
+     else{
+		echo '<script>alert("Login Failed: invalid password")</script>';
+	}
+
+	}/**
+	 * Logs in the user using the given username and password in the model.
+	 * @return boolean whether login is successful
+	 */
+	public function login()
+	{
+		if($this->_identity===null)
+		{
+			$this->_identity=new UserIdentity($this->user_name,$this->user_password);
+			$this->_identity->authenticate();
+		}
+		if($this->_identity->errorCode===UserIdentity::ERROR_NONE)
+		{
+			$duration=$this->rememberMe ? 3600*24*30 : 0; // 30 days
+			Yii::app()->user->login($this->_identity,$duration);
+			return true;
+		}
+		else
+			return false;
+	}
+}
---
+++ protected/views/site/login.php
@@ -0,0 +1,59 @@
+<?php
+/* @var $this SiteController */
+/* @var $model LoginForm */
+/* @var $form CActiveForm  */
+
+$this->pageTitle=Yii::app()->name . ' - Login';
+$this->breadcrumbs=array(
+	'Login',
+
+);
+
+?>
+
+<h1>Login</h1>
+
+<p>Please fill out the following form with your login credentials:</p>
+
+<div class="form">
+<?php $form=$this->beginWidget('CActiveForm', array(
+	'id'=>'login-form',
+	'enableClientValidation'=>true,
+	'clientOptions'=>array(
+		'validateOnSubmit'=>true,
+	),
+)); ?>
+
+	<p class="note">Fields with <span class="required">*</span> are required.</p>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'username'); ?>
+		<?php echo $form->textField($model,'user_name'); ?>
+		<?php echo $form->error($model,'user_name'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'password'); ?>
+		<?php echo $form->passwordField($model,'user_password'); ?>
+		<?php echo $form->error($model,'user_password'); ?>
+		<p class="hint">
+			Hint: You may login with <kbd>demo</kbd>/<kbd>demo</kbd> or <kbd>admin</kbd>/<kbd>admin</kbd>.
+		</p>
+	</div>
+
+	<div class="row rememberMe">
+		<?php echo $form->checkBox($model,'rememberMe'); ?>
+		<?php echo $form->label($model,'rememberMe'); ?>
+		<?php echo $form->error($model,'rememberMe'); ?>
+	</div>
+
+	<div class="row buttons">
+		<?php echo CHtml::submitButton('Login'); ?>
+	</div>
+
+	<div class="row ">
+		 <a href="http://localhost/Assignment_2-master/index.php?r=users/create">For new user click here</a>;
+	</div>
+
+<?php $this->endWidget(); ?>
+</div><!-- form -->
